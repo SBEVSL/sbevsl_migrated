5791451ed6124b948f88ad33a69170e55fd4d87d
Mario Rosa <vinnyrose@gmail.com>
2010-01-23 02:14:23 +0000

    Jess Templates are done, still some bugs in the motif checker and non of the new promol templates have been put in. Should not be used for testing.

--- promol/ProMol_dir/Tabs/motifs.py
+++ promol/ProMol_dir/Tabs/motifs.py
@@ -1,5 +1,5 @@
 from pymol import cmd
-from Tkinter import *
+import Tkinter as tk
 import Pmw
 from pmg_tk.startup.ProMol_dir import promolglobals as pglob
 from pmg_tk.startup.ProMol_dir.Methods.motif import *
@@ -7,167 +7,80 @@ from pmg_tk.startup.ProMol_dir.Methods.utility import *
 Pmw.initialise()
 
 def initialise():
-    #Molecular Classification Field
-    label = Label(pglob.Tabs['motifs']['tab'], text='Protein Classification')
-    label.grid(row=1,column=0,sticky='sw',padx=5,pady=0)
-    classify = Text(pglob.Tabs['motifs']['tab'], state=NORMAL, height=1.4, width=30)
-    classify.insert(END,"Protein Classification")
-    classify.grid(row=2, column=0, sticky='sw',padx=5, pady=0)
-
-    #Molecule Name Field
-    name = StringVar()
-    molName = Label(pglob.Tabs['motifs']['tab'], textvariable=name, font='arial, 14',
-        wraplength='450')
-    name.set("")
-    molName.grid(row=0, column=0, columnspan=2,sticky='w',padx=5, pady=5)
-
-
-    #Hetero Atoms
-    label=Label(pglob.Tabs['motifs']['tab'], text="Hetero Atoms")
-    label.grid(row=1, column=1, sticky='sw', padx=5, pady=0)
-    het = Text(pglob.Tabs['motifs']['tab'], state=NORMAL, height=1.4, width=25)
-    het.insert(END,"List of hetero atoms")
-    het.grid(row=2,column=1, sticky='w',padx=5,pady=0)
-    
-    #mechanisim group
-    group = Pmw.Group(pglob.Tabs['motifs']['tab'], tag_text = 'Motifs')
-    group.grid(row=3, column=0,columnspan =2, padx=0, pady=0)
-    interior = group.interior()
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Oxidoreductases:',
-            labelpos = 'n', items = ('','Superoxide Dismutase',
-            'Peroxidase', 'Alcohol Dehydrogenase', 'Aldose Reductase',
-            'Aldose Reductase CaCb', 'Aldose Reductase FA',
-            'NAD Reductase', 'NAD Reductase2',
-            'Betaine aldehyde dehydrogenase', 'Chondroitinase CaCb',
-            'Chondroitinase FA'),
-            menubutton_width = 8, command=oxidoreductase)
-    stereo.grid(row=0,column=0,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Transferases:',
-            labelpos = 'n', items = ('','Amino Transferase',
-            'Glutamine Amidotransferase','Thymidine Kinase', 'ACTase',
-            'Adenylate Kinase','SRC Family Kinase',
-            'Hhal Methyltransferase','Serotonin Acetyltransferase',
-            'Cyclin Dependent Kinase'),
-            menubutton_width = 8, command=transferase)
-    stereo.grid(row=0,column=1,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Hydrolases:',labelpos = 'n',
-             items = ('','Serine Protease', 'Serine Protease CaCb',
-             'Serine Protease FA','Papain Like', 'Metalloprotease',
-             'Tyrosine Phosphatase', 'Beta Lactamase', 'O-Glycosyl',
-             'Cephalosporin deacetylase'),
-             menubutton_width = 8, command=hydrolase)
-    stereo.grid(row=0,column=2,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Lyases:',labelpos = 'n',
-             items = ('','Carbonic Anhydrase', 'Carbon Carbon', 
-             'Chondroitinase', 'Hyaluronate-Lyase', 'Exonuclease 3', 
-             'Citrate Synthase'),
-             menubutton_width = 8, command=lyase)
-    stereo.grid(row=0,column=3,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Isomerases:',labelpos = 'n',
-             items = ('','Fucose Isomerase','Triose Phosphate Isomerase', 
-             'FK506 Cis-Trans'),
-             menubutton_width = 8, command=isomerase)
-    stereo.grid(row=1,column=0,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Ligase:',labelpos = 'n',
-             items = (' ', 'DNA Ligase'),
-             menubutton_width = 8, command=ligase)
-    stereo.grid(row=1,column=1,sticky=NW)
-
-    # Menu for motifs
-    stereo = Pmw.OptionMenu(interior,label_text = 'Other:',labelpos = 'n',
-             items = ('','Zinc Finger'),
-             menubutton_width = 8, command=other)
-    stereo.grid(row=1,column=2,sticky=NW)
-
-    stereo = Pmw.OptionMenu(interior,label_text = 'Options:',labelpos = 'n',
-                items = ('','Surface Pocket','Polar Contacts', 'Hide Contacts', 
-                'Show Substrate', 'Hide Substrate', 'Show label', 'Hide Label'),
-                menubutton_width = 8, command=motifoption)
-    stereo.grid(row=1,column=3,sticky=NW)
-    framerange = Frame(interior)
-    framerange.grid(row=2, column=1,columnspan = 4, padx=0, pady=0, sticky=NW)
-    ballrange = Pmw.Balloon(interior)
-    ballrange.bind(framerange, 'Multiplier for default measured values\n'+
-        'Re-click on desired motif to render change')
-    pglob.Tabs['motifs'].update({'delta':Scale(framerange, width =8)})
-    pglob.Tabs['motifs']['delta'].configure(troughcolor="#ffffff")
-    pglob.Tabs['motifs']['delta'].configure(length="200")
-    pglob.Tabs['motifs']['delta'].configure(orient="horizontal")
-    pglob.Tabs['motifs']['delta'].configure(resolution="0.01")
-    pglob.Tabs['motifs']['delta'].configure(to="2.0")
-    pglob.Tabs['motifs']['delta'].grid(row=2, column=1,columnspan = 4, padx=0, pady=0, sticky=NW)
-    pglob.Tabs['motifs']['delta'].set(1)
-
-    labrange = Label(interior, text='Precision Factor:')
-    labrange.grid(row=2, column=0, sticky=SE)
-
-    buttonAdd(interior, 'Reset', 10, resetrange, 3, 3, 'SE')
-
-
-
-    #---------------------Show residues around active site---------------#
-    group = Pmw.Group(pglob.Tabs['motifs']['tab'], tag_text='Tools')
-    group.grid(row=4, column=0, columnspan=1, padx=2, pady=2, sticky=W)
-    interior = group.interior()
-    framesela = Frame(interior)
-    framesela.grid(row=0, column=0, columnspan = 2, padx=0, pady=0, sticky=N)
-    ballsela = Pmw.Balloon(interior)
-    ballsela.bind(framesela, 'Within # Angstroms')
-    selA = Scale(framesela, width =8)
-    selA.configure(troughcolor="#ffffff")
-    selA.configure(length="130")
-    selA.configure(orient="horizontal")
-    selA.configure(resolution="0.2")
-    selA.configure(to="10.0")
-    selA.grid(row=0, column=0, columnspan= 2, padx=0, pady=0, sticky=N)
-    selA.set(5.0)
-    frameadj = Frame(interior)
-    frameadj.grid(row=1, column=0, padx=1, pady=1, sticky=NW)
-    balladj = Pmw.Balloon(interior)
-    balladj.bind(frameadj, 'Display residues adjacent to motif')
-
-
-    showround = Button(frameadj, width = 12, text = 'Adjacent')
-    showround.grid(row=1, column=0, padx=1, pady=1, sticky=NW)
-    showround.bind('<Button-1>', roundres)
-
-    delres = Button(interior, width = 14, text = 'Delete Adjacent')
-    delres.grid(row=1, column=1, padx=1, pady=1, sticky=NW)
-    delres.bind('<Button-1>', resdel)
-
     group = Pmw.Group(pglob.Tabs['motifs']['tab'], tag_text='Motif Search')
-    group.grid(row=4, column=1, columnspan=1, padx=2, pady=2, sticky=W)
+    group.grid(row=0, column=0, columnspan=1, padx=2, pady=2, sticky=tk.W)
     interior = group.interior()
 
     framemot = Frame(interior)
     framemot.grid(row = 0, column = 0)
     ballmot = Pmw.Balloon(interior)
-    ballmot.bind(framemot, 'Searches through all motifs\n1 = better, 2 = worse'+
+    ballmot.bind(framemot, 'Searches through all motifs\n0 = Motif not found, 1 = exact match, 2 = within range'+
         '\nDouble click on returns to show')
-    find_motif = Button(framemot, text ='Motif Finder')
+    find_motif = Button(framemot, text ='Motif Finder', command=motifchecker)
     find_motif.grid(row = 0, column = 0)
 
-    find_motif.bind('<Button-1>', motifchecker)
+    pglob.Tabs['motifs']['motifbox'] = Pmw.ScrolledListBox(interior,
+        items=(),
+        listbox_height = 6,
+        dblclickcommand=allmotif,
+        usehullsize = 1,
+        hull_width = 350,
+        hull_height = 400)
+    pglob.Tabs['motifs']['motifbox'].grid()
+    
+    group = Pmw.Group(pglob.Tabs['motifs']['tab'], tag_text='Tools')
+    group.grid(row=0, column=1, columnspan=1, padx=2, pady=2, sticky=tk.W)
+    interior = group.interior()
+    
+    labrange = Label(interior, text='Precision Factor:')
+    labrange.grid(row=1, column=0, sticky=tk.SW)
+        
+    rerange = Button(interior, text='Reset',highlightthickness=0, 
+                  width=8,command=resetrange,padx=2,pady=0)
+    rerange.grid(row=2, column=2, sticky=tk.SW, padx=2, pady=2)
 
+    framerange = Frame(interior)
+    framerange.grid(row=2, column=0,columnspan = 2, padx=0, pady=0, sticky=tk.N)
+    ballrange = Pmw.Balloon(interior)
+    ballrange.bind(framerange, 'Multiplier for default measured values\n'+
+        'Re-click on desired motif to render change')
+    pglob.Tabs['motifs'].update({'delta':Scale(framerange, width =8)})
+    pglob.Tabs['motifs']['delta'].configure(troughcolor="#ffffff")
+    pglob.Tabs['motifs']['delta'].configure(length="175")
+    pglob.Tabs['motifs']['delta'].configure(orient="horizontal")
+    pglob.Tabs['motifs']['delta'].configure(resolution="0.01")
+    pglob.Tabs['motifs']['delta'].configure(to="2.0")
+    pglob.Tabs['motifs']['delta'].grid(row=2, column=1,columnspan = 4, padx=0, pady=0, sticky=tk.N)
+    pglob.Tabs['motifs']['delta'].set(1)
+    
+    labadjacent = Label(interior, text='Adjacent:')
+    labadjacent.grid(row=3, column=0, sticky=tk.SW)
 
-    motifbox = Pmw.ScrolledListBox(interior,
-    items=(),
-    listbox_height = 6,
-    dblclickcommand=allmotif,
-    usehullsize = 1,
-    hull_width = 180,
-    hull_height = 100,)
-    #Changed hull_height to 100 to see more - Corey
-    motifbox.grid()
+    #---------------------Show residues around active site---------------#
+    
+    framesela = Frame(interior)
+    framesela.grid(row=4, column=0, columnspan = 2, padx=0, pady=0, sticky=tk.N)
+    ballsela = Pmw.Balloon(interior)
+    ballsela.bind(framesela, 'Within # Angstroms')
+    selA = Scale(framesela, width =8)
+    selA.configure(troughcolor="#ffffff")
+    selA.configure(length="175")
+    selA.configure(orient="horizontal")
+    selA.configure(resolution="0.2")
+    selA.configure(to="10.0")
+    selA.grid(row=4, column=0, columnspan=2, padx=0, pady=0, sticky=tk.N)
+    selA.set(5.0)
+
+    showround = Button(interior, width = 12, text = 'Adjacent', command=roundres)
+    showround.grid(row=5, column=0, columnspan=2, padx=1, pady=1, sticky=tk.NW)
+    balladj = Pmw.Balloon(interior)
+    balladj.bind(showround, 'Display residues adjacent to motif')
+
+    delres = Button(interior, width = 14, text = 'Delete Adjacent', command=resdel)
+    delres.grid(row=5, column=1, columnspan=2, padx=1, pady=1, sticky=tk.NW)
+    
+    stereo = Pmw.OptionMenu(interior,label_text = 'Options:',labelpos =tk.W,
+                items = ('','Surface Pocket','Polar Contacts', 'Hide Contacts', 
+                'Show Substrate', 'Hide Substrate', 'Show label', 'Hide Label'),
+                menubutton_width = 8, command=motifoption)
+    stereo.grid(row=6,column=0,sticky=tk.N)
