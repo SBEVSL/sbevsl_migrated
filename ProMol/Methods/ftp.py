66d97dd23d41e0fa2b164a34c5786e3fd78ab743
R. J. Bacon <bacon-rj@gmail.com>
2011-05-04 01:03:40 +0000

    Corrected Search Function and implemented some error catching

--- ProMol/Methods/ftp.py
+++ ProMol/Methods/ftp.py
@@ -13,22 +13,31 @@
 
 from pmg_tk.startup.ProMol import promolglobals as glb
 from ftplib import FTP
+from ftplib import all_errors
 import Tkinter as tk
+import tkMessageBox
 import re
 
 #Create the ftp instance
 ftp = FTP()
 
+#Listing of supported file types, to add a file type
+#just add it to this listing
+filetypes = ['pdb', 'ent', 'cif']
+
 
 def ftplistadd(dirline):
-    	glb.GUI.motifs['ftplist'].insert(tk.END, dirline)
+    glb.GUI.motifs['ftplist'].insert(tk.END, dirline)
     	
 def openftp(event = None):
     
     ftpaddress = glb.GUI.motifs['ftptxt'].get()
     global ftp 
-    ftp = FTP(ftpaddress)
-    ftp.login()
+    try:
+    	ftp = FTP(ftpaddress)
+    	ftp.login()
+    except all_errors: 
+    	tkMessageBox.showwarning("Error", "Could not connect to server.")
     glb.GUI.motifs['ftppath'].insert(tk.END, ftp.pwd())
     glb.GUI.motifs['ftplist'].delete(0, tk.END)
     ftp.retrlines('NLST', ftplistadd)
@@ -51,40 +60,19 @@ def ftpup():
     glb.GUI.motifs['ftplist'].delete(0, tk.END)
     ftp.retrlines('NLST', ftplistadd)
 
-def searchdirectory(filetree, timeout = 200):   
+def searchdirectory(directory, timeout = 200):   
     
-    thisdir = ftp.pwd() + '/'	
+    filetree = ftp.nlst(directory)
     timeout = timeout
     for loc in filetree:
-    	if loc.endswith(".pdb"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".pdb")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
-    	elif loc.endswith(".ent"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".ent")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
-    	elif loc.endswith(".ent.gz"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".ent.gz")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
-    	elif loc.endswith(".cif"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".cif")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
-    	elif loc.endswith(".pdb.gz"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".pdb.gz")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
-    	elif loc.endswith(".cif.gz"):
-    	       pdbfile = loc.lstrip("pdb")
-    	       pdbfile = pdbfile.rstrip(".cif.gz")
-    	       glb.GUI.motifs['multipdb'].insert(0.0, ", " + pdbfile)
+    	splits = loc.split(".")
+    	for split in splits:
+    		if split in filetypes:
+    			glb.GUI.motifs['multipdb'].insert(0.0, ", " + loc[3:7])
+    			break
     	else:
-    	 	    newtree = []
-    	 	    thisdir = thisdir + loc
-    		    newtree = ftp.nlst(thisdir)
-    		    searchdirectory(newtree, timeout)
+    		thisdir = directory + '/' + loc
+    		searchdirectory(thisdir, timeout)
     	if timeout <= 0:
     	 	break
     	timeout = timeout - 1
@@ -95,12 +83,11 @@ def openftpdir():
     index = glb.GUI.motifs['ftplist'].curselection()[0] 
     directory = glb.GUI.motifs['ftplist'].get(index)
     print("Opened FTP directory: '" + ftp.pwd() + '/' + directory + "'")
-    filetree = ftp.nlst(ftp.pwd() + '/' + directory)
     ftp.cwd(ftp.pwd() + '/' + directory)
     glb.GUI.motifs['ftppath'].delete(0, tk.END)
     glb.GUI.motifs['ftppath'].insert(tk.END, ftp.pwd() + '/')
     glb.GUI.motifs['multipdb'].delete(0.0, tk.END)
-    searchdirectory(filetree,  int(glb.GUI.motifs['toval'].get()) )
+    searchdirectory(ftp.pwd(),  int(glb.GUI.motifs['toval'].get()) )
     glb.GUI.motifs['multipdb'].delete(0.0, 1.2)
     ftpup()
     
