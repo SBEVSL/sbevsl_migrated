9413050a2f2922854b2afa29654808cfc8f8762d
Shariq S. Madha <ssmadha@hotmail.com>
2014-06-24 13:49:45 +0000

    Adding metal motifs to my version of ProMol

---
+++ ProMol/Motifs/M_1hxq_2_7_7_12.py
@@ -0,0 +1,16 @@
+'''
+FUNC:M_1hxq_2_7_7_12
+PDB:1hxq
+EC:2.7.7.12
+RESI:zn,fe
+LOCI:a-350,351;
+'''
+cmd.select('zn1', 'n. ZN&r. zn w. %s of n. FE&r. fe'%(d*31.79))
+cmd.select('zn',' br. zn1')
+cmd.delete('zn1')
+cmd.select('fe1', 'n. FE&r. fe w. %s of n. ZN&zn'%(d*31.79))
+cmd.select('fe',' br. fe1')
+cmd.delete('fe1')
+cmd.select('U_1hxq_2_7_7_12', 'zn|fe')
+cmd.delete('zn')
+cmd.delete('fe')
