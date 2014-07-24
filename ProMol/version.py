3eee2c8f33f6ac29a8e33e8156d9b62e546dbd45
Herbert J. Bernstein <yayahjb@gmail.com>
2014-07-24 15:48:59 +0000

    Update to algorithm 1.2, changing Levenshtein distance filter to be based on the minimum distance relative to the length os the motif rather than the maximum. Change the background for alignment images to white. Update the Users Guide as editied by T. McKay.   -- HJB

--- ProMol/version.py
+++ ProMol/version.py
@@ -1,7 +1,7 @@
 # PLEASE try to keep this constant up to date with each commit!
-VERSION = '5.3-r347'
+VERSION = '5.3-r362'
 
-ALG_VERSION = '1.1' 
+ALG_VERSION = '1.2' 
 
 # The following variable controls whether this version includes
 # use of the JESS motifs
